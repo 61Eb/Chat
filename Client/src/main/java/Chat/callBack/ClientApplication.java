3e5b6e1631e1850eddd3b8605d449b178ee4c66e
gleb <gleb22ka@gmail.com>
2024-03-16 20:05:08 +0300

    ClientApplication

--- Client/src/main/java/Chat/callBack/ClientApplication.java
+++ Client/src/main/java/Chat/callBack/ClientApplication.java
@@ -4,22 +4,26 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.sql.*;
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class ClientApplication {
 
-    String url      = "jdbc:mysql://127.0.0.1:3306/?user=root";
-    String dbName   = "database auntification";
-    String userName = "root";
-    String password = "87654321";
+    public static void validateLogin(PreparedStatement pstmt) {
+        try {
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                System.out.println("Login successful!");
+            } else {
+                System.out.println("Invalid credentials. Login failed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        try (
-                Socket socket = new Socket("localhost", 8189);
-                DataInputStream in = new DataInputStream(socket.getInputStream());
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+        try (Socket socket = new Socket("localhost", 8189); DataInputStream in = new DataInputStream(socket.getInputStream()); DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
             System.out.println("Подключились к серверу");
             Scanner scanner = new Scanner(System.in);
 
@@ -44,55 +48,6 @@ public class ClientApplication {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
-        Connection conn = null;
-        long st = System.currentTimeMillis();
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gleb?user=root + database auntification", "root", "87654321");
-            log("---get connection in " + (System.currentTimeMillis() - st) + "(ms)");
-            st = System.currentTimeMillis();
-            if (conn != null)
-                System.out.println("Приложение подключилось к БД !");
-            else
-                System.out.println("Приложение НЕ подключилось к БД ?");
-
-            int userId = 2;
-            String userPwd = "654321";
-
-
-            String sql = "select UserId, UserName, Pwd from Users where UserId=? and Pwd=?";
-            java.sql.PreparedStatement stmt = conn.prepareStatement(sql);
-            log("---prepare stmt in " + (System.currentTimeMillis() - st) + "(ms)");
-            st = System.currentTimeMillis();
-            stmt.setInt(1, userId);
-            stmt.setString(2, userPwd);
-
-            log("--try execute stmt: " + stmt);
-
-            java.sql.ResultSet rs = stmt.executeQuery();
-            log("---executeQuery in " + (System.currentTimeMillis() - st) + "(ms)");
-            st = System.currentTimeMillis();
-            log("--rs:" + rs);
-            boolean isUserExist = false;
-            if (rs.next()) {
-                isUserExist = true;
-                log("--UserId: " + rs.getInt("UserId"));
-                log("--UserName: " + rs.getString("UserName"));
-                log("--Pwd: " + rs.getString("Pwd"));
-
-            }
-            log("---isUserExist: " + isUserExist);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (conn != null && !conn.isClosed()) {
-                    conn.close();
-                }
-            } catch (Exception ex1) {
-                ex1.printStackTrace();
-            }
         }
     }
 
--- Server/src/main/java/chat/server/ServerApplication.java
+++ Server/src/main/java/chat/server/ServerApplication.java
@@ -1,10 +1,65 @@
 package chat.server;
 
+import java.sql.*;
+
 public class ServerApplication {
     public ServerApplication() {
     }
+
     public static void main(String[] args) {
         Server server = new Server(8189);
         server.start();
+        Connection conn = null;
+        long st = System.currentTimeMillis();
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gleb?user=root + database auntification", "root", "87654321");
+            log("---get connection in " + (System.currentTimeMillis() - st) + "(ms)");
+            st = System.currentTimeMillis();
+            if (conn != null)
+                System.out.println("Приложение подключилось к БД !");
+            else
+                System.out.println("Приложение НЕ подключилось к БД ?");
+
+            int userId = 2;
+            String userPwd = "654321";
+
+
+            String sql = "select UserId, UserName, Pwd from Users where UserId=? and Pwd=?";
+            java.sql.PreparedStatement stmt = conn.prepareStatement(sql);
+            log("---prepare stmt in " + (System.currentTimeMillis() - st) + "(ms)");
+            st = System.currentTimeMillis();
+            stmt.setInt(1, userId);
+            stmt.setString(2, userPwd);
+
+            log("--try execute stmt: " + stmt);
+
+            java.sql.ResultSet rs = stmt.executeQuery();
+            log("---executeQuery in " + (System.currentTimeMillis() - st) + "(ms)");
+            st = System.currentTimeMillis();
+            log("--rs:" + rs);
+            boolean isUserExist = false;
+            if (rs.next()) {
+                isUserExist = true;
+                log("--UserId: " + rs.getInt("UserId"));
+                log("--UserName: " + rs.getString("UserName"));
+                log("--Pwd: " + rs.getString("Pwd"));
+
+            }
+            log("---isUserExist: " + isUserExist);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (Exception ex1) {
+                ex1.printStackTrace();
+            }
+        }
+    }
+
+    private static void log(String str) {
+        System.out.println(str);
     }
 }
